aa3b314641ce9a9a67bb36431d5747adbdac2213
One-percent-athlete <one.percent.athlete@gmail.com>
2024-11-14 12:06:56 +0900

    Import shadcn ui

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
-      <h1 className="text-3xl underline">Next Care</h1>
+      <h1 className="text-3xl text-white underline">Next Care</h1>
     </div>
   );
 }
