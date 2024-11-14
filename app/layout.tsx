3d7163e81585410c09df01b81d966baf70b691e1
One-percent-athlete <one.percent.athlete@gmail.com>
2024-11-14 17:29:44 +0900

    Update public folder

--- app/layout.tsx
+++ app/layout.tsx
@@ -43,8 +43,6 @@ export default function RootLayout({
           >
             {children}
           </ThemeProvider>
-
-        {children}
       </body>
     </html>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -3,11 +3,12 @@ import { Button } from "@/components/ui/button"
 
 export default function Home() {
   return (
-    <div>
-      
-      <h1 className="text-3xl text-white underline">Next Care</h1>
-      <Button>Click me</Button>
-
+    <div className="flex h-screen max-h-screen">
+      <section className="remove-scrollbar container my-auto">
+        <div className="sub-container max-w-[496px]">
+          <Image src="/assets/icons/logo-full.svg" alt="logo" height={1000} width={1000} />
+        </div>
+      </section>
     </div>
   );
 }
