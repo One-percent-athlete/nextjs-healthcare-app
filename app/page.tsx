1a7217ebbc86b8b081792032b63e47e4d4f5df31
One-percent-athlete <one.percent.athlete@gmail.com>
2024-11-14 17:01:31 +0900

    Update utils.ts

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
 import Image from "next/image";
+import { Button } from "@/components/ui/button"
 
 export default function Home() {
   return (
     <div>
       <h1 className="text-3xl text-white underline">Next Care</h1>
+      <Button>Click me</Button>
+
     </div>
   );
 }
