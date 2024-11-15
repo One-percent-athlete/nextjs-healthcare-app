9d87e705da2f39ca7273412e05812db41f6b0a1b
One-percent-athlete <one.percent.athlete@gmail.com>
2024-11-15 18:17:28 +0900

    Create simple form

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
     <div className="flex h-screen max-h-screen">
       <section className="remove-scrollbar container my-auto">
         <div className="sub-container max-w-[496px]">
-          <Image src="/assets/icons/logo-full.svg" alt="logo" height={1000} width={1000} />
+          <Image src="/assets/icons/logo-full.svg" alt="logo" height={1000} width={1000} className="mb-12 h-10 w-fit"/>
         </div>
       </section>
     </div>
