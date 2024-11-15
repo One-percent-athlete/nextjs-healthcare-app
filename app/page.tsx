13f6b486d63d4b772a864309e97f16ac8cbef93c
One-percent-athlete <one.percent.athlete@gmail.com>
2024-11-15 18:18:42 +0900

    Render form on homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import { Button } from "@/components/ui/button"
+import PacientForm from "@/components/forms/PacientForm";
 
 export default function Home() {
   return (
@@ -7,6 +8,8 @@ export default function Home() {
       <section className="remove-scrollbar container my-auto">
         <div className="sub-container max-w-[496px]">
           <Image src="/assets/icons/logo-full.svg" alt="logo" height={1000} width={1000} className="mb-12 h-10 w-fit"/>
+
+          <PacientForm />
         </div>
       </section>
     </div>
